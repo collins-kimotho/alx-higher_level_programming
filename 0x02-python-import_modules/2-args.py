dae98c983f07b35ce0c7a91bb7104111c1eb7191
collins-kimotho <collinskimotho16@gmail.com>
2023-12-03 21:40:43 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     if argc == 0:
         print("{} arguments.".format(argc))
     elif argc == 1:
-        print("{} argument.".format(argc))
+        print("{} argument:".format(argc))
     else:
-        print("{} arguments.".format(argc))
+        print("{} arguments:".format(argc))
 
     for i in range(1, argc + 1):
-        print("{} : {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
