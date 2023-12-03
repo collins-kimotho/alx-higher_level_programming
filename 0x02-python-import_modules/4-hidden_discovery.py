d0b39e79e73579b1018d002a3b42ea68a0166f98
collins-kimotho <collinskimotho16@gmail.com>
2023-12-03 22:03:06 +0000

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-    for name in dir(hidden_4):
-        if name[0] != '_' and name[1] != '_':
+    for name in sorted(dir(hidden_4)):
+        if not name.startswith("__"):
             print(name)
