a3856815d1fb483a84683cc538dd8af8b203d3dc
collins-kimotho <collinskimotho16@gmail.com>
2023-12-03 21:23:44 +0000

    calculatior

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,4 +4,7 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    print(f"{a} + {b} = {add(a, b)}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
