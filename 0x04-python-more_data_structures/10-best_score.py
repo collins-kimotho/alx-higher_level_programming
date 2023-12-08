e14f09bf1a481a0aa26235f261583cc06e59a937
collins-kimotho <collinskimotho16@gmail.com>
2023-12-08 11:30:49 +0000

    best-score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary or all(value is None for value in a_dictionary.values()):
+    if not a_dictionary:
         return None
+    if all(value is None for value in a_dictionary.values()):
+        return None
+
     return max(a_dictionary, key=a_dictionary.get)
