b24c5c65976cbae1d545998a065d7fd2e28f593b
collins-kimotho <collinskimotho16@gmail.com>
2024-01-12 22:05:19 +0000

    copylist

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def copy_list(l):
+    """
+    Returns a copy of a list.
+
+    Args:
+        l: A list of any type of objects.
+
+    Returns:
+        A copy of the input list.
+
+    Raises:
+        None.
+    """
+    return l.copy()
