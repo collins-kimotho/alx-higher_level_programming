432608e8c824aac1f7af3ea350835ebabcd1ea13
collins-kimotho <collinskimotho16@gmail.com>
2024-01-13 09:41:44 +0000

    lockedclass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""This module is for a locked class."""
 
 
 class LockedClass:
-    """
-    Prevenst user from dynamically creating new instance
-    """
+    """Prevenst user from dynamically creating new instance."""
+
     __slots__ = ['first_name']
