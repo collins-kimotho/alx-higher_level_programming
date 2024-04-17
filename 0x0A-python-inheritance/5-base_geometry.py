83ed99b162fe46f24c07dc64ad594d1d074f013e
collins-kimotho <collinskimotho16@gmail.com>
2024-04-17 07:44:26 +0000

    base

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is the '5-base_geometry' module"""
+
+
+class BaseGeometry:
+    """
+    This is the 'BaseGeometry' class.
+
+    This class is currently empty and serves a base class for future classes
+    """
+    pass
