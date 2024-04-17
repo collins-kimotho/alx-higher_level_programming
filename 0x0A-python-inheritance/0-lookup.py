f15a50f7897e822b304ff1689079b4a10b19b73c
collins-kimotho <collinskimotho16@gmail.com>
2024-04-17 05:19:01 +0000

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+    Returns:
+        A list of the names of the available attributes and methods of the obj
+    """
+    return dir(obj)
