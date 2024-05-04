3101c6fd898c1730342db874790e9ebfacabaa6f
collins-kimotho <collinskimotho16@gmail.com>
2024-05-04 19:37:24 +0000

    addfile

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -19,3 +19,25 @@ def save_to_json_file(my_obj, filename):
     """
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(my_obj, f)
+
+    def load_from_json_file(filename):
+        """
+        Creates an object from a JSON file.
+
+        Args:
+            filename (str): The name of the JSON file to read from.
+
+        Returns:
+            The python data structure rep by the JSON file.
+        """
+
+    filename = "add_tem.json"
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
+
+    for arg in sys.argv[1:]:
+        my_list.append(arg)
+
+    save_to_json_file(my_list, filename)
