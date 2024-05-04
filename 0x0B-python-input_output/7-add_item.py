f50062dfa8f4af6d3e4b4e6dcf4be8843d6e6516
collins-kimotho <collinskimotho16@gmail.com>
2024-05-04 19:40:59 +0000

    additem

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,43 +1,18 @@
 #!/usr/bin/python3
-"""
-Module containing script that adds all args to a python list
-"""
+"""Moddule containing custom function."""
 import sys
-import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def save_to_json_file(my_obj, filename):
-    """
-    Writes an obj to a text file, using JSON rep
-
-    Args:
-        my_obj: The object to write to the file
-        filename (str): The name fo the file to write to.
-
-    Returns:
-        None
-    """
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
-
-    def load_from_json_file(filename):
-        """
-        Creates an object from a JSON file.
-
-        Args:
-            filename (str): The name of the JSON file to read from.
-
-        Returns:
-            The python data structure rep by the JSON file.
-        """
-
-    filename = "add_tem.json"
+if __name__ == "__main__":
+    filename = "add_item.json"
+    data = []
     try:
-        my_list = load_from_json_file(filename)
-    except FileNotFoundError:
-        my_list = []
+        data = load_from_json_file(filename)
+    except Exception as e:
+        pass
 
     for arg in sys.argv[1:]:
-        my_list.append(arg)
+        data.append(arg)
 
-    save_to_json_file(my_list, filename)
+    save_to_json_file(data, filename)
