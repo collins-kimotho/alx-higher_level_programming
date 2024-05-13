c9c589ceec87ef563130de35533c2bc5ebd928cd
collins-kimotho <collinskimotho16@gmail.com>
2024-05-13 22:58:40 +0000

    public method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,7 @@ class Rectangle(Base):
         """Method that prints the rect using '#"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Returns the string rep of the rectangle instance"""
+        return "[Rectangle] ({} {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
