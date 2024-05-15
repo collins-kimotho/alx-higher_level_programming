c3e5cb781b9ecf24b5f073da2c870eddef8c46ba
collins-kimotho <collinskimotho16@gmail.com>
2024-05-15 09:37:17 +0000

    dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Modile for Base Class"""
+import json
 
 
 class Base:
@@ -13,3 +14,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string rep of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
