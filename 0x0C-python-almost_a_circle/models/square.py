2f04b654b40d80f660929ccf473a66a6044922ba
collins-kimotho <collinskimotho16@gmail.com>
2024-05-15 04:32:48 +0000

    assign attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,8 +9,36 @@ class Square(Rectangle):
         """Class constructor"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Method that returns the string rep of the square instance"""
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x, self.y,
                                                  self.width)
+
+    def update(self, *args, **kwargs):
+        """Method that assigns an argument to each attribute"""
+        attributes = ['id', 'size', 'x', 'y']
+        if args and len(args) > 0:
+            for i, args in enumerate(args):
+                if i < len(attributes):
+                    if attributes[i] == 'size':
+                        self.size = args
+                else:
+                    setattr(self, attributes[i], arg)
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    self.size = value
+                elif key in attributes:
+                    setattr(self, key, value)
