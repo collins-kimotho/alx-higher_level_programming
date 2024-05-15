b255d43d3810081ae10bc6596506c9a425218308
collins-kimotho <collinskimotho16@gmail.com>
2024-05-15 08:52:55 +0000

    public method update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -36,3 +36,7 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary rep of a Square"""
+        return {'id': self.id, 'size':self.size, 'x': self.x, 'y': self.y}
