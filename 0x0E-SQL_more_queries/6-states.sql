6da82662645b2f3a090955b0cc2f19a2f843b8f0
collins-kimotho <collinskimotho16@gmail.com>
2024-05-23 09:22:24 +0000

    table and database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -3,11 +3,8 @@
 -- Create the database if it doesn't exist
 CREAT DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Switch to the database
-USE hbtn_0d_usa;
-
 -- Create the table if it doesn't exist
-CREATE TABLE IF NOT EXISTS `hbtn_od_usa.states` (
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.states` (
 	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	name VARCHAR(256) NOT NULL
 );
