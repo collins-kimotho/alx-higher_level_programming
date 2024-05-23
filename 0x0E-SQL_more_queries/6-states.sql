111ca29338cb38752cb0da5cb2b500017c0656d7
collins-kimotho <collinskimotho16@gmail.com>
2024-05-23 09:42:18 +0000

    new database new table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,14 @@
--- Creates the table hbtn_0d_usa with table states.
-CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-    PRIMARY KEY(`id`),
-    `id`   INT          NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(256) NOT NULL
+-- Script that creates a new database and new table
+
+-- Create the new datbase if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Switch to the database
+USE hbtn_0d_usa;
+
+-- Create the states table if it doesn't exist
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
 );
