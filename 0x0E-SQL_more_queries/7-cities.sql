8f81eb51670c0605136914ed38939a93731454c8
collins-kimotho <collinskimotho16@gmail.com>
2024-05-23 09:48:08 +0000

    primary key and foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- Script that creates a new database and a new table
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- SWitch to the new database
+USE hbtn_0d_usa;
+
+-- Create the new table if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
