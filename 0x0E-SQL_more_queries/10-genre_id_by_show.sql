bf05f5a09e832223c9b01b5ae1543244cdd6d1c8
collins-kimotho <collinskimotho16@gmail.com>
2024-05-23 10:46:17 +0000

    left

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
--- Script to list all shows with at least one genre linked
+-- Script to list all shows in the database hbtn_0d_tvshows
+
+-- All your SQL queries should have a comment just before (i.e. syntax above)
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
