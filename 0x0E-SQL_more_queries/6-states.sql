5f1ad5d2ea439613bdaa32676d8191fec76172de
collins-kimotho <collinskimotho16@gmail.com>
2024-05-23 01:39:54 +0000

    bring back use code

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,9 @@
 -- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Use the hbtn database
+USE hbtn_0d_usa;
+
 -- Create the sates table
 CREATE TABLE IF NOT EXISTS states (
 	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
