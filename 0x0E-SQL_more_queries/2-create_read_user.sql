bd246d8ca1fc2fbe3586ace053c682c50d564301
collins-kimotho <collinskimotho16@gmail.com>
2024-05-23 00:56:23 +0000

    spelling mistake corrected

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS htbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
