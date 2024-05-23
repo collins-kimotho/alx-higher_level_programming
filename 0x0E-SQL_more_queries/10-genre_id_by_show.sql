716146acb95fd2be991e1383067eb1d8927dcc1a
collins-kimotho <collinskimotho16@gmail.com>
2024-05-23 10:52:04 +0000

    order by

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,5 @@
 -- Script to list all shows in the database hbtn_0d_tvshows
-
--- All your SQL queries should have a comment just before (i.e. syntax above)
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT s.title, g.genre_id
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
+ORDER BY s.title, g.genre_id;
