5060011bf0ea9338876d73748874761760fb2a8d
collins-kimotho <collinskimotho16@gmail.com>
2024-06-24 03:26:17 +0000

    add constructor and instance attributes

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+class Rectangle {
+  // Constructor and other methods will go here
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
