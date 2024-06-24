7316e52af1b8521e61594003daa7755422a4418e
collins-kimotho <collinskimotho16@gmail.com>
2024-06-24 11:40:16 +0000

    convert a number from base 10

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-
-let numArgs = 0; // initialize the count
-
-exports.logMe = function (item) {
-  console.log(`${numArgs} : ${item}`);
-  numArgs++; // increment the count
-};
+let count = 0;
+exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
