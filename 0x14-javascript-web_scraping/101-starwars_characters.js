8d344eca84807e6f560027697294c1ef15ce3cd0
collins-kimotho <collinskimotho16@gmail.com>
2024-07-30 06:52:59 -0700

    starwars

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,33 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request.get(url, (error, response, body) => {
+// Make a GET request to the Star Wars API for the given movie ID
+request.get(apiUrl, { json: true }, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
+    console.error('Error:', error);
+    return;
+  }
+
+  // Check if the data contains a characters field
+  if (body && Array.isArray(body.characters)) {
+    // Fetch each character's details and print their names
+    body.characters.forEach((characterUrl) => {
+      request.get(characterUrl, { json: true }, (err, res, characterBody) => {
+        if (err) {
+          console.error('Error:', err);
+          return;
+        }
+
+        // Print the character's name
+        if (characterBody && characterBody.name) {
+          console.log(characterBody.name);
         }
       });
-    }
+    });
+  } else {
+    console.log('No characters found for this movie.');
   }
 });
