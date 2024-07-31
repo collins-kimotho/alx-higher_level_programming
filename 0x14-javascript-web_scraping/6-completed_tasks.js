fdf675b479e16b50d6a28b86b63e917dac4db8c7
collins-kimotho <collinskimotho16@gmail.com>
2024-07-30 23:30:24 -0700

    right-order

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,24 @@
 #!/usr/bin/node
-/**
-  compute number of tasks completed by user id
-  usage: /6-completed_tasks.js <API URL>
-  */
-const myArgs = process.argv.slice(2);
+// computes the number of tasks completed by user
+
 const request = require('request');
-const id_ = [];
-const result = {};
-request(myArgs[0], function (error, response, body) {
-  let i;
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
   if (!error) {
-    const json_ = JSON.parse(body);
-    const len = json_.length;
-    for (i = 0; i < len; i++) {
-      if (json_[i].completed) {
-        id_.push(json_[i].userId);
+    const todos = JSON.parse(body);
+    const completed = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completed[todo.userId] === undefined) {
+          completed[todo.userId] = 1;
+        } else {
+          completed[todo.userId] += 1;
+        }
       }
-    }
-    for (let j = 0; j < id_.length; ++j) {
-      if (!result[id_[j]]) { result[id_[j]] = 0; }
-      ++result[id_[j]];
-    }
+    });
+
+    console.log(completed);
   }
-  console.log(result);
 });
